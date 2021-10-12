69be1fb7ac2faa2c09ea222b50eeca48a56e518f
Zacqwerty <67268484+Zacqwerty@users.noreply.github.com>
2021-10-12 21:32:20 +0800

    fixed some css

--- home.php
+++ home.php
@@ -19,15 +19,16 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" href="Resources/css/lnInp.css" type="text/css">
         <link rel="stylesheet" href="Resources/css/task.css" type="text/css">
+        <link rel="stylesheet" href="Resources/css/card_Boards.css" type="text/css">
     </head>
     <body style="background-color: rgb(245, 245, 245);">
       <!--SIDEBAR-->
     <div style="width: 300px; height: 100%; position: fixed; top: 0; left: 0%; text-align: left; background-color: rgb(67, 67, 126);">
 
       <img src="./Resources/imgs/logo.png" width="300px" height="130px" style="margin-top: 15px; margin-bottom: 35px;">
-      <div class="ss" onclick="window.location.href = 'home.php'">Home</div>
-      <div class="ss" onclick="window.location.href = 'account.php'"> Account</div>
-      <div class="ss" onclick="window.location.href = 'content.php';">Shared Files</div>
+      <div class="sidebarBtn" onclick="window.location.href = 'home.php'">Home</div>
+      <div class="sidebarBtn" onclick="window.location.href = 'account.php'"> Account</div>
+      <div class="sidebarBtn" onclick="window.location.href = 'content.php';">Shared Files</div>
 
        <form action="logout.php" method="post" target="_self">
          <button
@@ -47,18 +48,18 @@
 <div class="col-divider" style="display:flex; height: auto;">
   <div class="" style=" margin-left: 300px; top: 0; left: 0; height: 100%; width: 100%; position:relative; display: flex;">
     <?php
-    if(mysqli_num_rows($sqlPNPUnitCards) > 0){
-      while($rowsPNPUnitCards  = mysqli_fetch_assoc($sqlPNPUnitCards)){
-        echo '<div class="card" id="card" style="width: 300px; height: 340px; margin: 15px; border-radius: 12px; border: 1px rgb(175, 175, 175) solid; display=flex; align-content: left;">
-            <img width="900px" height="175px" class="card-img-top" style="border-top-left-radius: 12px; border-top-right-radius: 12px;" src="https://th.bing.com/th/id/OIP.SbXGF2KkQYG5Ggz6RG7IxAHaHa?pid=ImgDet&rs=1">
-            <div class="card-body">
-              <h5 class="card-title">'.$rowsPNPUnitCards['pnp_unit_name'].'</h5>
-              <a href="'.$rowsPNPUnitCards['video_conference_link'].'" class="btn btn-primary" style="margin-bottom: 1px;">Go to Conference</a>
-            </div>
-          </div>';
-          }
+      if(mysqli_num_rows($sqlPNPUnitCards) > 0){
+        while($rowsPNPUnitCards  = mysqli_fetch_assoc($sqlPNPUnitCards)){
+          echo '<div class="card deptCard" id="card">
+              <img width="900px" height="175px" class="card-img-top imgDept" src="https://th.bing.com/th/id/OIP.SbXGF2KkQYG5Ggz6RG7IxAHaHa?pid=ImgDet&rs=1">
+              <div class="card-body">
+                <h5 class="card-title">'.$rowsPNPUnitCards['pnp_unit_name'].'</h5>
+                <a href="'.$rowsPNPUnitCards['video_conference_link'].'" class="btn btn-primary" style="margin-bottom: 1px;">Go to Conference</a>
+              </div>
+            </div>';
         }
-?>
+      }
+    ?>
   </div>
   <div class="card-main">
     <h5 style="color: white; width: 100%;">Tasks to do:</h5>
@@ -69,11 +70,8 @@
     <div class="card-insideD" style="margin: 0; padding: 0;">
       <iframe src="listtask.php" frameborder="0" width="100%" height="200"style="border-radius: inherit;"></iframe>
     </div>
-
-          </div>
-</div>
-
-
+    </div>
+    </div>
 
     </body>
 </html>
